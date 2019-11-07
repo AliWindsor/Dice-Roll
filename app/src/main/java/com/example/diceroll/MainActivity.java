d5f5b78a33a1fbcd6b0ffed29ca3a8d4e2a7438d
AliWindsor <aliwindsor@gmail.com>
2019-11-07 18:23:02 +0000

    REDO: Part 1 level 1

--- app/src/main/java/com/example/diceroll/MainActivity.java
+++ app/src/main/java/com/example/diceroll/MainActivity.java
@@ -1,77 +1,87 @@
 package com.example.diceroll;
 
-import android.app.Activity;
-import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.example.diceroll.MESSAGE";
-    public static final String EXTRA_MESSAGE2 = "com.example.diceroll.MESSAGE2";
-    public static final String EXTRA_MESSAGE3 = "com.example.diceroll.MESSAGE3";
-    @Override
+    TextView die;
+    TextView score;
+    TextView winLoseTxt;
+    Button rollButton;
+    EditText editText;
 
+    int counter = 0;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        //get view from layout ting
         setContentView(R.layout.activity_main);
+
     }
 
-    public static int randomClass(){
+    public static int randomClass() {
         Random randomGenerator = new Random();
         int randomInt = randomGenerator.nextInt(6) + 1;
         return randomInt;
     }
 
-//    public String rightWrong(){
+//    @SuppressLint("ResourceType")
+//    public String winOrLose() {
 //
-//        String message = "b";
-//        String roll = "b";
+//        score = findViewById(R.id.score);
+//
+//        editText = findViewById(R.id.editText);
+//        String guess = editText.getText().toString();
+//
+//        String roll = Integer.toString(randomClass());
+//        die = findViewById(R.id.die);
+//        die.setText(roll);
+//
 //        String winLose;
 //
-//        if (message == roll){
-//            winLose = "Win";
-//        }else{
-//            winLose ="Lose";
+//        int guessIt = Integer.parseInt(guess);
+//        int rollNo = Integer.parseInt(roll);
+//
+//        if (guessIt == rollNo) {
+//            winLoseTxt.setTextColor(getResources().getColor(android.R.color.holo_green_light));
+//            winLose = "Congratulations!!";
+//            //a1();
+//            counter++;
+//            score.setText(Integer.toString(counter));
+//
+//        } else {
+//            winLoseTxt.setTextColor(getResources().getColor(android.R.color.holo_red_light));
+//            winLose = "Try Again :(";
 //        }
 //
-//        System.out.println(winLose);
-//        return (winLose);
+//        return winLose;
 //    }
 
-    /** Called when the user taps the Send button */
     public void sendMessage(View view) {
-        // Do something in response to button
 
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE2, message);
+//
+//        score = findViewById(R.id.score);
+//
+//        editText = findViewById(R.id.editText);
+//        String guess = editText.getText().toString();
 
         String roll = Integer.toString(randomClass());
-        //String roll = "3";
-        intent.putExtra(EXTRA_MESSAGE, roll);
+        die = findViewById(R.id.die);
+        die.setText(roll);
 
-        String winLose;
-        int guessIt = Integer.parseInt(message);
-        int rollNo = Integer.parseInt(roll);
+//        winLoseTxt = findViewById(R.id.winLoseTxt);
+//        winLoseTxt.setText(winOrLose());
 
-        if (guessIt == rollNo){
-            winLose = "Congratulations!!.";
-        }else {
-            winLose = "Try Again :(";
-        }
-
-        intent.putExtra(EXTRA_MESSAGE3, winLose);
-        startActivity(intent);
     }
-
-
 }
+
